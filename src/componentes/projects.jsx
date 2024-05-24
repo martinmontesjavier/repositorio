26729fc1b4ea274a50bff6c33942914eeccb229e
martinmontesjavier <120597788+martinmontesjavier@users.noreply.github.com>
2024-05-24 15:39:18 +0200

    Add files via upload

--- src/componentes/projects.jsx
+++ src/componentes/projects.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import tetrisImage from '../assets/proyecto-tetris.png';
-import ticketsImage from '../assets/proyecto-tickets.png';
-import memoryImage from '../assets/proyecto-memory.png';
+import tetrisImage from '../imagenes/proyecto-tetris.png';
+import ticketsImage from '../imagenes/proyecto-tickets.png';
+import memoryImage from '../imagenes/proyecto-memory.png';
 
 
 function Project({ title, description, imageSrc, projectUrl }) {
@@ -45,20 +45,20 @@ export function Projects() {
     {
       title: "Tetris",
       description: "Esta es una versión mia y personalizada del legendario juego de Tetris, desarrollada completamente en JavaScript utilizando un enfoque de componentes y vistas. Inspirado en el clásico de los 80, este proyecto combina la nostalgia del juego original con la flexibilidad y la interactividad que proporcionan las tecnologías web contemporáneas.",
-      imageSrc: "src/assets/proyecto-tetris.png",
-      projectUrl: tetrisImage
+      imageSrc: tetrisImage,
+      projectUrl: "https://tetris-definitivo.onrender.com/"
     },
     {
       title: "Gestión de tickets",
       description: "Esta es una aplicación de gestión de incidencias desarrollada en JavaScript que ofrece una solución eficiente para gestionar y resolver incidencias en cualquier entorno, desde equipos de desarrollo hasta departamentos de atención al cliente. Este proyecto, construido con un enfoque modular y orientado a componentes y vistas, proporciona herramientas específicas para cada rol dentro de la organización, facilitando la colaboración y mejorando la productividad en la resolución de problemas.",
-      imageSrc: "src/assets/proyecto-tickets.png",
-      projectUrl: ticketsImage
+      imageSrc: ticketsImage,
+      projectUrl: "https://tickets-ounj.onrender.com/"
     },
     {
       title: "Pokemon Memory",
       description: "PokeMemory es un juego de memoria inspirado en el mundo de Pokémon, desarrollado con React.js. Este proyecto combina la nostalgia de la franquicia de Pokémon con la potencia y la flexibilidad del framework React, utilizando componentes y vistas reutilizables, contextos para la gestión del estado global, peticiones a la API de Pokémon para obtener datos y efectos y estados para una experiencia de juego dinámica y envolvente.",
-      imageSrc: "src/assets/proyecto-memory.png",
-      projectUrl: memoryImage
+      imageSrc: memoryImage,
+      projectUrl: "https://memory-jwwm.onrender.com/"
     }
   ];
 
